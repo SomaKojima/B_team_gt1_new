71fa76fb079696a0d0f70cfd71d0b85ed3d51933
yoshiki hamaguchi <amakinton1996916@gmail.com>
2019-11-29 12:25:47 +0900

    コミット

---
+++ Katon_Collection/Assets/Scripts/ServerInterface/SI_Player.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SI_Player : MonoBehaviour
+{
+    private int[] placePoint;
+    private int[] itemCount;
+    private int id;
+    private new string name;
+    private bool isExcange;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public int GetPlacePoint(int index)
+    {
+        return placePoint[index];
+    }
+
+    public void SetPlacePoint(int point,int index)
+    {
+        placePoint[index] = point;
+    }
+
+    public int GetItemCount(int index)
+    {
+        return itemCount[index];
+    }
+
+    public void SetItemCount(int point, int index)
+    {
+        itemCount[index] = point;
+    }
+
+    public int ID
+    {
+        get { return id; }
+        set { id = value; }
+    }
+}
--- Katon_Collection/Assets/Scripts/ServerInterface/SI_Room.cs
+++ Katon_Collection/Assets/Scripts/ServerInterface/SI_Room.cs
@@ -2,8 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SI_Room : MonoBehaviour
+public class SI_Room : Photon.MonoBehaviour
 {
+    private int currentPlayer;
+    private int maxPlayer;
+    private string roomName;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +18,23 @@ public class SI_Room : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public int CurrentPlayer
+    {
+        get { return currentPlayer; }
+        set { currentPlayer = value; }
+    }
+
+    public int MaxPlayer
+    {
+        get { return maxPlayer; }
+        set { maxPlayer = value; }
+    }
+
+    public string RoomName
+    {
+        get { return roomName; }
+        set { roomName = value; }
     }
 }
