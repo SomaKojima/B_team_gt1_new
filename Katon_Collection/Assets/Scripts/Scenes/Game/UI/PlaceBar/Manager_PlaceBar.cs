123ac1a58b8fab8bd56c397786d7ad36340286c0
HiroyaInahaki <hiroyainagaki2018@gmail.com>
2019-11-29 10:57:46 +0900

    PlaceBar、フォルダー追加

---
+++ Katon_Collection/Assets/Scripts/Scenes/Game/UI/PlaceBar/Manager_PlaceBar.cs
@@ -0,0 +1,166 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Manager_PlaceBar : MonoBehaviour
+{
+    //拠点の個数
+    const int BASE_NUM = 4;
+
+
+    Type m_placeType;
+
+    [SerializeField]
+    Image m_pencil;
+    
+    [SerializeField]
+    //拠点ボタン
+    UI_Button m_bases;
+
+    [SerializeField]
+    //市場ボタン
+    UI_Button m_ichiba;
+
+    [SerializeField]
+    //噴水ボタン
+    UI_Button m_fountain;
+
+    [SerializeField]
+    //カメラボタン
+    UI_Button m_camera;
+
+    [SerializeField]
+    //拠点の場所
+    UI_Button[] m_placeButtons = new UI_Button[BASE_NUM];
+
+    //カメラが押されたか
+    bool m_isQRLeader = false;
+
+    //アクティブな状態か
+    bool m_active = false;
+    
+
+
+    // Update is called once per frame
+    void Update(){
+
+
+        
+        ClickEvent();
+    }
+
+    //ボタンをクリックしたときにタイプを設定
+    void ClickEvent()
+    {
+        //拠点をクリックしたら
+        if (m_bases.IsClick())
+        {
+            m_bases.OnClickProcess();
+
+            m_active = !m_active;
+
+
+            for (int i = 0; i < BASE_NUM; i++)
+            {
+                if(m_active)
+                {
+                    m_placeButtons[i].gameObject.SetActive(true);
+                    m_pencil.gameObject.SetActive(true);
+                }
+                else
+                {
+                    m_placeButtons[i].gameObject.SetActive(false);
+                    m_pencil.gameObject.SetActive(false);
+                }
+               
+            }
+
+        }
+
+        //森
+        if (m_placeButtons[0].IsClick())
+        {
+            m_placeButtons[0].OnClickProcess();
+
+            m_placeType = Type.forest;
+
+            Debug.Log(m_placeType);
+        }
+
+        //洞窟
+        if (m_placeButtons[1].IsClick())
+        {
+            m_placeButtons[1].OnClickProcess();
+
+            m_placeType = Type.cave;
+
+            Debug.Log(m_placeType);
+        }
+
+        //工場
+        if (m_placeButtons[2].IsClick())
+        {
+            m_placeButtons[2].OnClickProcess();
+
+            m_placeType =Type.factory;
+
+            Debug.Log(m_placeType);
+        }
+
+        //牧場
+        if (m_placeButtons[3].IsClick())
+        {
+            m_placeButtons[3].OnClickProcess();
+
+            m_placeType = Type.farm;
+
+            Debug.Log(m_placeType);
+        }
+
+
+
+        //市場ボタンを押したら
+        if (m_ichiba.IsClick())
+        {
+            m_ichiba.OnClickProcess();
+
+            m_placeType = Type.market;
+
+            Debug.Log(m_placeType);
+        }
+
+        //噴水ボタンを押したら
+        if (m_fountain.IsClick())
+        {
+            m_fountain.OnClickProcess();
+
+            m_placeType = Type.fountain;
+
+            Debug.Log(m_placeType);
+        }
+
+        //カメラボタンを押したら
+        if (m_camera.IsClick())
+        {
+            m_camera.OnClickProcess();
+
+            m_isQRLeader = true;
+
+           
+        }
+    }
+
+
+    //タイプの取得
+    public Type GetchangeType()
+    {
+        return m_placeType;
+    }
+
+    //カメラを起動したかを取得
+    public bool GetIsQRLeader()
+    {
+        return m_isQRLeader;
+    }
+}
