6f1f1231b8eec8f4623ade584872035da1ee3b0f
WadaRiku <poke.rikuyaya09@gmail.com>
2019-12-18 12:23:04 +0900

    result背景追加

---
+++ Katon_Collection/Assets/Scripts/Scenes/Result/Floor/Factory_Floor_Result.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Factory_Floor_Result : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject prefab;
+    [SerializeField]
+    private Transform parent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public Floor_Result Create()
+    {
+        GameObject instance = Instantiate(prefab);
+        instance.transform.SetParent(parent, false);
+        Floor_Result floor_result = instance.GetComponent<Floor_Result>();
+        return floor_result;
+    }
+}
---
+++ Katon_Collection/Assets/Scripts/Scenes/Result/ResultGame.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResultGame : MonoBehaviour
+{
+    [SerializeField]
+    Congratulation congratulation;
+    [SerializeField]
+    RankIcon rankIcon;
+    [SerializeField]
+    Manager_Floor_Result manager_Floor_Result;
+    [SerializeField]
+    Factory_Floor_Result factory_Floor_Result;
+    [SerializeField]
+    UI_Button_GoToTitle ui_Button_GoToTitle;
+    [SerializeField]
+    CameraResultMove cameraResultMove;
+
+    float time = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time++;
+        if(time > 60)
+        {
+
+            time = 0;
+        }
+    }
+}
