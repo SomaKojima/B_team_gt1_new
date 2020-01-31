51dff234bd5960c87fd086f02335b4ae288db40e
yoshiki hamaguchi <amakinton1996916@gmail.com>
2020-01-31 11:27:45 +0900

    同期処理、修正１

--- Katon_Collection/Assets/Scripts/Scenes/Game/UI/PLInfo/PLInfoManager.cs
+++ Katon_Collection/Assets/Scripts/Scenes/Game/UI/PLInfo/PLInfoManager.cs
@@ -37,19 +37,22 @@ public class PLInfoManager : MonoBehaviour
     {
         //Prefabの読み込み
         GameObject prefab = (GameObject)Resources.Load("Prefabs/Scenes/Game/UI/PLInfo/PLInfoWindow");
-        PhotonPlayer[] playerList = PhotonNetwork.playerList;
         PLInfoWindows = new GameObject[data.Count];
-
 
         for (int i = 0; i < data.Count; i++)
         {
             PLInfoWindows[i] = Instantiate(prefab, this.transform.position, Quaternion.identity);
             PLInfoWindows[i].transform.parent = playerWindows.transform;
             PLInfoWindows[i].transform.localScale = new Vector3(1, 1, 1);
-            PLInfoWindows[i].GetComponent<PLInfoWindow>().SetNameText(playerList[i].NickName);
+            PLInfoWindows[i].GetComponent<PLInfoWindow>().SetNameText(data[i].name);
             PLInfoWindows[i].GetComponent<PLInfoWindow>().SetItemData(itemManager);
             PLInfoWindows[i].GetComponent<PLInfoWindow>().DataSet(data[i]);
             PLInfoWindows[i].GetComponent<PLInfoWindow>().createList();
+
+            if(data[i].ID == PhotonNetwork.player.ID)
+            {
+                currentNumber = i;
+            }
         }
         isActive = true;
         BtnSetActive(true);
